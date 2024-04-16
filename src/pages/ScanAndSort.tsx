5dab201593dc21928e613288e3f620473fe0da08
DESKTOP-557UREB\Ivy <ivytsai2@gmail.com>
2024-04-16 10:15:14 -0400

    scan and sort page background image added

--- src/pages/ScanAndSort.tsx
+++ src/pages/ScanAndSort.tsx
@@ -1,6 +1,8 @@
 import { Camera, CameraResultType } from '@capacitor/camera';
 import { IonBackButton, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React, { useState } from 'react';
+import './ScanAndSort.css';
+import CameraIcon from '../assets/camera.png';
 
 const ScanAndSort: React.FC = () => {
     const [image, setImage] = useState<any>(null);
@@ -25,11 +27,20 @@ const ScanAndSort: React.FC = () => {
                     <IonTitle>WasteWise</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className="ion-padding">
-                <h1>Scan and Sort</h1>
-                <IonButton expand='full' onClick={takePic}>
-                    Take Picture
+            <IonContent className="ion-padding ion-text-center" class='background-img'>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <IonButton fill='clear' onClick={takePic}>
+                    <img src={CameraIcon} alt='Camera Icon'/>
                 </IonButton>
+                <br />
+                <br />
+                <h1><b>Sort yout waste item</b></h1>
                 <img src={image} alt='' />
             </IonContent>
         </IonPage>
