edf5b8fba3a1b577b08fcb4e74b92fb04941fad9
DESKTOP-557UREB\Ivy <ivytsai2@gmail.com>
2024-04-17 04:41:12 -0400

    scan and sort page bounding box

--- src/pages/ScanAndSort.tsx
+++ src/pages/ScanAndSort.tsx
@@ -7,7 +7,7 @@ import CameraIcon from '../assets/camera.png';
 const ScanAndSort: React.FC = () => {
     const [image, setImage] = useState<any>(null);
     const [result, setResult] = useState<any>(null);
-    //const [boundingBoxes, setBoundingBoxes] = useState(null);
+    const [category, setCategory] = useState('');
     const takePic = async () => {
         const image = await Camera.getPhoto({
             quality: 90,
@@ -35,7 +35,24 @@ const ScanAndSort: React.FC = () => {
         // Parse the JSON response
         const data = await response.json();
         setResult(data);
+        console.log(data);
         console.log('Predicted outcome:', result);
+
+        const getCategory = (item: string) => {
+            switch (item.toLowerCase()) {
+                case 'plastic':
+                    return 'blue';
+                case 'apple':
+                    return 'green';
+                case 'trash':
+                    return 'black';
+                default:
+                    return 'other';
+           }
+        };
+    
+        const cat = getCategory(result['item']);
+        setCategory(cat)
     };
 
     return (
@@ -63,25 +80,28 @@ const ScanAndSort: React.FC = () => {
                 <br />
                 <br />
                 <h1><b>Sort yout waste item</b></h1>
-                <div id='image-div'>
-                    <img src={image} alt='Captured Image' />
-                    {result && (                      
-                        <div 
+                {result ? (    
+                    <div id='image-div'>
+                        <img src={image} alt='Captured Image' />                  
+                        <div
                             style={{
                                 position: 'absolute',
-                                top: `18px`,
-                                left: `68px`,
-                                width: `255px`,
-                                height: `340px`,
-                                border: '2px solid red',
+                                top: `${result['ymin']}px`,
+                                left: `${result['xmin']}px`,
+                                width: `${result['xmax'] - result['xmin']}px`,
+                                height: `${result['ymax'] - result['ymin']}px`,
+                                border: `5px solid ${category}`,
                                 boxSizing: 'border-box',
-                            }}>
-                        </div>                       
-                    )}
-                </div>
-                {result && (<div>{result['item']}</div>)}
-                
-            </IonContent>
+                            }}><p 
+                                style={{
+                                    color: `${category}`,
+                                    fontWeight: 'bold' 
+                                }}
+                            >{category.toUpperCase()} BIN</p>
+                        </div>   
+                    </div>                  
+                ):<div></div>}
+            </IonContent> 
         </IonPage>
     );
 };
