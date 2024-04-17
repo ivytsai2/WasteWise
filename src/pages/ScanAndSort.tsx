69091e149bb28b5ba216cbf8bd33a5b4c70d15f0
DESKTOP-557UREB\Ivy <ivytsai2@gmail.com>
2024-04-17 17:57:54 -0400

    scan and sort page complete

--- src/pages/ScanAndSort.tsx
+++ src/pages/ScanAndSort.tsx
@@ -1,61 +1,106 @@
 import { Camera, CameraResultType } from '@capacitor/camera';
 import { IonBackButton, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ScanAndSort.css';
 import CameraIcon from '../assets/camera.png';
 
 const ScanAndSort: React.FC = () => {
     const [image, setImage] = useState<any>(null);
+    const [originalWidth, setOriginalWidth] = useState<number>(0);
+    const [originalHeight, setOriginalHeight] = useState<number>(0);
     const [result, setResult] = useState<any>(null);
     const [category, setCategory] = useState('');
+
+    const getCategory = (item: string) => {
+        switch (item.toLowerCase()) {
+            case 'plastic':
+            case 'cans':
+            case 'tins':
+            case 'metal_trays_plates_pans':
+            case 'cardboard':
+            case 'styrofoam container':
+            case 'envelope':
+            case 'paper':
+            case 'carton':
+                return 'blue';
+            case 'organics':
+                return 'green';
+            case 'general':
+                return 'black';
+            default:
+                return 'other';
+       }
+    };
+
     const takePic = async () => {
-        const image = await Camera.getPhoto({
-            quality: 90,
-            allowEditing: false,
-            resultType: CameraResultType.Base64
-        });
+        try {
+            const image = await Camera.getPhoto({
+                quality: 90,
+                allowEditing: false,
+                resultType: CameraResultType.Base64
+            });
 
-        const img = `data:image/jpeg;base64,${image.base64String}`;
-        setImage(img);
+            const img = `data:image/jpeg;base64,${image.base64String}`;
+            setImage(img);
 
-        const formData = new FormData();
-        formData.append('image', image.base64String);
+            // Create an HTML image element to get the original width and height
+            const tempImage = new Image();
+            tempImage.src = img;
+            tempImage.onload = () => {
+                setOriginalWidth(tempImage.width);
+                setOriginalHeight(tempImage.height);
+            };
 
-        const requestOptions = {
-            method: 'POST',
-            body: formData
-        };
-        const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        const serverUrl = 'http://ec2-18-233-100-133.compute-1.amazonaws.com:8080/wasteClassify';
-        const response = await fetch(corsProxyUrl + serverUrl, requestOptions);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
+            const formData = new FormData();
+            formData.append('image', image.base64String);
+            //formData.append('test', "True");
 
-        // Parse the JSON response
-        const data = await response.json();
-        setResult(data);
-        console.log(data);
-        console.log('Predicted outcome:', result);
+            const requestOptions = {
+                method: 'POST',
+                body: formData
+            };
+            const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+            const serverUrl = 'http://ec2-54-166-194-21.compute-1.amazonaws.com:8080/wasteClassify';
+            const response = await fetch(corsProxyUrl + serverUrl, requestOptions);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            } else {
+                const data = await response.json();
+                const data_array = data.split(",")
+                const data_object = {
+                    xmin: data_array[0],
+                    ymin: data_array[1],
+                    xmax: data_array[2],
+                    ymax: data_array[3],
+                    class: data_array[4],
+                    score: data_array[5],
+                }
+                setResult(data_object);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }        
+    };
 
-        const getCategory = (item: string) => {
-            switch (item.toLowerCase()) {
-                case 'plastic':
-                    return 'blue';
-                case 'apple':
-                    return 'green';
-                case 'trash':
-                    return 'black';
-                default:
-                    return 'other';
-           }
-        };
-        
+    const newWidth = 416;
+    const newHeight = 416;
+    const xScale = newWidth / originalWidth;
+    const yScale = newHeight / originalHeight;
+
+
+    useEffect(() => {
         if (result) {
-            const cat = getCategory(result['item']);
+            const cat = getCategory(result.class);
             setCategory(cat)
         }
-    };
+    }, [result]);
+    
+    useEffect(() => {
+        console.log('Original Width:', originalWidth);
+        console.log('Original Height:', originalHeight);
+        console.log('Predicted outcome:', result);
+        console.log('Category:', category);
+    }, [originalWidth, originalHeight, result, category]);
 
     return (
         <IonPage>
@@ -84,14 +129,15 @@ const ScanAndSort: React.FC = () => {
                 <h1><b>Sort yout waste item</b></h1>
                 {result ? (    
                     <div id='image-div'>
-                        <img src={image} alt='Captured Image' />                  
+                        <img src={image} alt='Captured Image' /> 
+                        {category != 'other' ? (             
                         <div
                             style={{
                                 position: 'absolute',
-                                top: `${result['ymin']}px`,
-                                left: `${result['xmin']}px`,
-                                width: `${result['xmax'] - result['xmin']}px`,
-                                height: `${result['ymax'] - result['ymin']}px`,
+                                top: `${result['ymin']*yScale}px`,
+                                left: `${result['xmin']*xScale}px`,
+                                width: `${(result['xmax'] - result['xmin'])*xScale}px`,
+                                height: `${(result['ymax'] - result['ymin'])*yScale}px`,
                                 border: `5px solid ${category}`,
                                 boxSizing: 'border-box',
                             }}><p 
@@ -100,7 +146,10 @@ const ScanAndSort: React.FC = () => {
                                     fontWeight: 'bold' 
                                 }}
                             >{category.toUpperCase()} BIN</p>
-                        </div>   
+                        </div> ) : <h2 style={{
+                                        textShadow: '2px 2px #eb3474'}}>
+                                    <b>Ask chatbot for dispose method.</b></h2>
+                        }  
                     </div>                  
                 ):<div></div>}
             </IonContent> 
