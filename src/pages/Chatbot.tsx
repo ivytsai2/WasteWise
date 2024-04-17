322c4bfa9fb13b1c8d442a412be551ef3a4539d5
DESKTOP-557UREB\Ivy <ivytsai2@gmail.com>
2024-04-17 04:41:57 -0400

    sacn and sort page bounding box part2

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 
 const Chatbot: React.FC = () => {
@@ -6,12 +6,16 @@ const Chatbot: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Chatbot</IonTitle>
+                <IonToolbar color={'success'}>
+                    <IonButton slot='start' color={'success'}>
+                        <IonBackButton>
+                        </IonBackButton>
+                    </IonButton>
+                    <IonTitle>WasteWise</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
-                UI goes here...
+                <h1>Chatbot</h1>
             </IonContent>
         </IonPage>
     );
